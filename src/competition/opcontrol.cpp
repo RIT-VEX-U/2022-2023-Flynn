5d3633f2e6868808db3313cc23925245e044a1d8
cowsed <richie.sommers@outlook.com>
2022-10-02 13:07:53 -0400

    Commented out old printf s Terminal is cleaner now

--- src/competition/opcontrol.cpp
+++ src/competition/opcontrol.cpp
@@ -1,12 +1,17 @@
 #include "competition/opcontrol.h"
-
+#include "C:/Users/richi/VEX/2022-2023-Flynn/include/robot-config.h"
+#include <iostream>
 /**
  * Contains the main loop of the robot code while running in the driver-control period.
  */
+
+
 void opcontrol()
 {
   // Initialization
-
+  main_controller.ButtonA.pressed([](){printf("a pressed");});
+  main_controller.ButtonA.released([](){printf("a released");});
+  
 
   // Periodic
   while(true)
@@ -19,7 +24,6 @@ void opcontrol()
     // ========== SECONDARY REMOTE ==========
 
     // ========== AUTOMATION ==========    
-
 
     // Wait 20 milliseconds for control loops to calculate time correctly
     vexDelay(20);
