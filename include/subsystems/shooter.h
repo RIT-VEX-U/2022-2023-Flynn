f6001f855f01411f8b188d498867d665245eec50
Christopher Nokes <crn2267@rit.edu>
2022-10-23 15:41:49 -0400

    Further improved documentation, neatening up.

--- include/subsystems/shooter.h
+++ include/subsystems/shooter.h
@@ -4,34 +4,32 @@ class Shooter{
   public:
   Shooter(Flywheel *flywheel, limit &indexer_limit, motor &indexer_motor, digital_out &pressure_bar, motor &intake_motor);
 
-  void fire();
-  void stop_firing();
-  void startIntaking();
-  void stopIntaking();
-  void spinAt(double RPM);
-  void stopSpinning();
-  double targetRPM();
-  double getRPM();
+  void fire();                  // set the indexer motor to go forward to fire disks
+  void stop_firing();           // stop the indexer motor
+  void startIntaking();         // start the intake motor
+  void stopIntaking();          // stop the intake motor
+  void spinAt(double RPM);      // spin the flywheel at a given RPM.
+  void stopSpinning();          // stop the flywheel and its tasks.
+  double getRPM();              // get the current RPM of the flywheel.
 
-  void applyPressure();
-  void releasePressure();
+  void applyPressure();         // applies pressure (?)
+  void releasePressure();       // releases pressure (?)
 
-  bool getJustFired();
-  void setJustFired(bool);
+  bool getJustFired();          // returns whether or not the indexer was just set in motion
+  void setJustFired(bool);      // sets the just_fired variable
 
-  bool indexer_switch_state();
-  double getDesiredRPM();
-  void shootAllRPM(int RPM);
+  bool indexer_switch_state();  // checks whether or not the indexer is back at its starting position
+  double getDesiredRPM();       // get the desired RPM of the flywheel.
+  void shootAllRPM(int RPM);    // shoots all disks in the robot at a given RPM.
 
-  void startWatchingTrigger();
-  void stopWatchingTrigger();
-  void stopShootAll();
-  void setFireRunning(bool val);
-  void resetIndexer();
-  bool inAcceptableRPMRange();
+  void startWatchingTrigger();  // starts the trigger_watching_task.
+  void stopWatchingTrigger();   // stops the trigger_watching_task.
+  void stopShootAll();          // stops the fire_task and resumes the trigger_watching_task.
+  void setFireRunning(bool val);// sets the fire_all_running boolean to a given value.
+  void resetIndexer();          // resets the indexer to its starting position. 
+  bool inAcceptableRPMRange();  // checks to see if the actual RPM is within +-10 of the desired RPM.
 
   private:
-
   //Shooty wheels
   Flywheel *flywheel;
   //Shooty trigger
@@ -43,8 +41,8 @@ class Shooter{
   motor &intake_motor;
 
   bool just_fired = true; //whether or not the command to fire has been dealt with
-  bool trigger_watcher_running = false;
-  bool fire_all_running = false;
-  task trigger_watcher_task;
-  task fire_task;
+  bool trigger_watcher_running = false; // whether or not the trigger_watcher task is running.
+  bool fire_all_running = false;        // whether or not the fire task is running.
+  task trigger_watcher_task;            // handles manual use, firing, resetting.
+  task fire_task;                       // handles firing all disks currently held.
 };
