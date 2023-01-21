3cd574c0294af1d155f9261ce4de56730a2857f2
Ryan McGee <ramtech51@comcast.net>
2023-01-20 19:57:46 -0500

    Added hardware and subsystem definitions

--- include/robot-config.h
+++ include/robot-config.h
@@ -8,13 +8,30 @@ extern brain Brain;
 extern controller main_controller;
 
 // ======== OUTPUTS ========
+extern motor left_front, left_mid, left_rear;
+extern motor right_front, right_mid, right_rear;
+extern motor intake, roller, flywheel;
+extern motor_group left_motors, right_motors;
+extern motor_group flywheel_motors;
 
 // ======== INPUTS ========
-
-// ======== SUBSYSTEMS ========
+extern CustomEncoder left_enc, right_enc, mid_enc;
 
 // ======== UTILS ========
+// Drive Tuning
+extern Odometry3Wheel::odometry3wheel_cfg_t odometry_cfg;
 extern robot_specs_t config;
+
+// Flywheel Tuning
+extern FeedForward::ff_config_t flywheel_ff_cfg;
+extern PID::pid_config_t flywheel_pid_cfg;
+
+// ======== SUBSYSTEMS ========
+extern Odometry3Wheel odometry_sys;
+extern TankDrive drive_sys;
+extern Flywheel flywheel_sys;
+
+
 
 /**
  * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
--- src/robot-config.cpp
+++ src/robot-config.cpp
@@ -7,15 +7,54 @@ brain Brain;
 
 // ======== OUTPUTS ========
 
-// ======== INPUTS ========
+// Drive
+motor left_front(PORT20), left_mid(PORT8), left_rear(PORT9);
+motor right_front(PORT11, true), right_mid(PORT1, true), right_rear(PORT2, true);
 
-// ======== SUBSYSTEMS ========
+motor_group left_motors(left_front, left_mid, left_rear);
+motor_group right_motors(right_front, right_mid, right_rear);
+
+// Manipulation
+motor flywheel(PORT12);
+motor intake(PORT19);
+motor roller(PORT18);
+
+motor_group flywheel_motors(flywheel);
+
+// ======== INPUTS ========
+CustomEncoder left_enc(Brain.ThreeWirePort.A, 2048);
+CustomEncoder mid_enc(Brain.ThreeWirePort.C, 2048);
+CustomEncoder right_enc(Brain.ThreeWirePort.E, 2048);
 
 // ======== UTILS ========
+// Odometry Tuning
+Odometry3Wheel::odometry3wheel_cfg_t odometry_cfg = {
 
+};
+
+// Drive Tuning
 robot_specs_t config = {
 
 };
+
+// Flywheel Tuning
+FeedForward::ff_config_t flywheel_ff_cfg = {
+
+};
+
+PID::pid_config_t flywheel_pid_cfg = {
+    
+};
+
+// ======== SUBSYSTEMS ========
+
+Odometry3Wheel odometry_sys(left_enc, right_enc, mid_enc, odometry_cfg);
+
+TankDrive drive_sys(left_motors, right_motors, config, &odometry_sys);
+
+Flywheel flywheel_sys(flywheel_motors, flywheel_pid_cfg, flywheel_ff_cfg, 1.0);
+
+
 
 /**
  * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
